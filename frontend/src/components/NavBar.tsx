2487208caba1c50753976273a1266cdb55f617f9
HoustonBoston <myhorsefly12345@gmail.com>
2025-07-14 21:02:51 -0700

    Centered the search bar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -5,15 +5,16 @@ import {Input} from "@heroui/input"
 function NavBar() {
     return (
         <header className="shadow shadow-neutral-500 fixed transition-all duration-100 w-full h-12 bg-neutral-200">
-            <div className="flex flex-row w-full h-full items-center text-cyan-600">
+            <div className="flex flex-row w-full h-full items-center">
                 <div className="ml-auto">
                     <Input 
                         placeholder="Movie"
-                        radius="full"
-                        variant="bordered"
+                        radius="lg"
+                        className="ml-[50%] bg-amber-200"
+                        type="search"
                     />
                 </div>
-                <div className="space-x-10 ml-auto">
+                <div className="space-x-10 ml-auto text-cyan-600">
                     <Link href="/">Home</Link>
                     <Link href="/movies">Movies</Link>
                     <Link href="/login" className="pr-2">Login</Link>
