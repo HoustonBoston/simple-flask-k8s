1b145b7a0049533af4573ee7b3bde584f7ccce28
HoustonBoston <myhorsefly12345@gmail.com>
2025-07-14 18:02:06 -0700

    Centered the search bar, but only the right end of the bar is in the middle, center the midpoint of the search bar.

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -6,14 +6,14 @@ function NavBar() {
     return (
         <header className="shadow shadow-neutral-500 fixed transition-all duration-100 w-full h-12 bg-neutral-200">
             <div className="flex flex-row w-full h-full items-center text-cyan-600">
-                <div className="justify-center">
+                <div className="ml-auto">
                     <Input 
                         placeholder="Movie"
                         radius="full"
                         variant="bordered"
                     />
                 </div>
-                <div className="space-x-10">
+                <div className="space-x-10 ml-auto">
                     <Link href="/">Home</Link>
                     <Link href="/movies">Movies</Link>
                     <Link href="/login" className="pr-2">Login</Link>
